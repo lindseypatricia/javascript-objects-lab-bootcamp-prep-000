4f1fd31835baaee8cc3534e71f885d3a3d6e017d
Lindsey Brown <brown.lindsey.p@gmail.com>
2018-07-01 05:28:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
 
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  
+  [key]value
 }
 
 function deleteFromObjectByKey(object, key) {
