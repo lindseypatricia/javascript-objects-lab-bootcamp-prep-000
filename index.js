032714fd208f81348fbd04d3c955bf75fbbd77fd
Lindsey Brown <brown.lindsey.p@gmail.com>
2018-07-01 05:24:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ var recipes = {
 function updateObjectWithKeyAndValue(object, key, value) {
   
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  
+}
+
+function deleteFromObjectByKey(object, key) {
+  
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  
+}
